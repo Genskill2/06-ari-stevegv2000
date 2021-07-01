846683428eca5534b4e4b7c22c482e1f0c89354e
Steve George Varghese <stevegv2000@gmail.com>
2021-07-02 02:55:44 +0400

    Submission of Assignment 06 ARI_stevegv2000

---
+++ ari.c
@@ -0,0 +1,21 @@
+string ari(string s) {
+int characters=0;
+int words=0;
+int sentences=0;
+int ari;
+char* gradelevel[14] = {"Kindergarten", "First/Second Grade", "Third Grade", "Fourth Grade", "Fifth Grade", "Sixth Grade", "Seventh Grade", "Eighth Grade", "Ninth Grade", "Tenth Grade", "Eleventh Grade", "Twelfth grade", "College Student", "Professor"};
+for(int i=0; i < strlen(s); i++) {
+    if(isalnum(s[i])) {
+        characters++;
+    }
+    else if(s[i]==' '){
+        words++;
+    }
+    else if(s[i]=='.' || s[i]=='!' || s[i]=='?') {
+        sentences++;
+    }
+ari = 4.71*((float)characters/words) + 0.5*((float)words/sentences) - 21.43;
+}
+//printf("%s\n", gradelevel[ari]);
+return gradelevel[ari];
+}
